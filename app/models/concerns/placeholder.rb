2126bef63b1cc224d5e1ccd4bef0f7b6988885c8
mat5098 <mezyk50@gmail.com>
2022-07-05 16:36:06 +0200

    Added concern for managing image data

---
+++ app/models/concerns/placeholder.rb
@@ -0,0 +1,9 @@
+module Placeholder
+    extend ActiveSupport::Concern
+    def self.image_generator(height:, width:)
+        "https://placehold.co/#{height}x#{width}"
+        
+    end
+    
+
+end
